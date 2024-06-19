c58855db9faf9ad89ad0584ee5cdef6650c9042e
Mohan <mohan@wesrom.com>
2024-06-19 22:43:25 +0530

    env read and pm2 installed

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const {createServer } = require('http');
+require('dotenv').config();
 const hostName = '127.0.0.1';
-const port = '3000';
+const port = process.env.PORT || 3000;
 
 const server = createServer((req,res) =>{
     res.statusCode = 200;
     res.setHeader('Content-Type','text/plain');
-    res.end('Hello World');
+    res.end('Hello World PM2 installed');
 });
 
 server.listen(port,hostName, () => {
